d84f27a173492ff1dab491eda33ea21a1c0370f2
Jahid Walid <jahidwalidme@gmail.com>
2023-01-18 19:24:27 +0600

    Fainal Project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 fetch('https://randomuser.me/api/').
 then(res => res.json()).
-then(data => console.log(data))
+then(data => userData(data));
+
+function userData(userInfo){
+    const h1=document.getElementById('username');
+    const img=document.getElementById('userimg');
+    console.log(userInfo);
+    h1.innerText=`${userInfo.results[0].name.title} ${userInfo.results[0].name.first} ${userInfo.results[0].name.last}`;
+
+    img.src=`${userInfo.results[0].picture.large}`;
+    console.log(img)
+}
+
+setInterval(() => {
+    location.reload()
+}, 3000);
+
